62a7f36ee550b3f7f9eca00e17e043a18cfa4bb0
Maymeric <aymeric.marec@gmail.com>
2025-01-13 09:56:11 +0100

    arguments

--- ids.py
+++ ids.py
@@ -1,9 +1,24 @@
 import argparse
+from build import build
+from check import check
 
-parser = argparse.ArgumentParser(description='pute')
+def IDS():
+    parser = argparse.ArgumentParser(description='pute')
 
-parser.add_argument('-b', '--build',action='store_true') 
-parser.add_argument('-c', '--check',action='store_true')
-
-args = parser.parse_args()
-print(args.build,args.check)
+    parser.add_argument('-b', '--build',action='store_true') 
+    parser.add_argument('-c', '--check',action='store_true')
+    try:
+        args = parser.parse_args()
+    except:
+        parser.print_help()
+        return
+    if(not args.build and not args.check):
+        print("Nice argument bro , kys next time")
+        parser.print_help()
+    if(args.build):
+        build()
+    if(args.check):
+        check()
+    
+if __name__ == "__main__":
+    IDS()
